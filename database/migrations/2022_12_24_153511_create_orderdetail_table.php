4ef5bbf1725a0469558acc991dbb4da0dfd90793
surakasem <surakasemzaaz@gmail.com>
2022-12-24 23:16:30 +0700

    db ongoing 50%
    create db by er firsttest

---
+++ database/migrations/2022_12_24_153511_create_orderdetail_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderdetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orderdetail', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('productid');
+            $table->unsignedBigInteger('orderid');
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orderdetail');
+    }
+}
